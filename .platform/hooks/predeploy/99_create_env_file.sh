faece487bcdfdc286d0301819a96bc20d5fda82b
stevewolfe5555 <stevewolfe71@gmail.com>
2023-11-28 22:08:50 +0000

    Try running script in staging directory

--- .platform/hooks/predeploy/99_create_env_file.sh
+++ .platform/hooks/predeploy/99_create_env_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
       # Copy .env.example to .env if it doesn't exist
-      if [ ! -f /var/app/current/.env ]; then
-        cp /var/app/current/.env.example /var/app/current/.env
+      if [ ! -f /var/app/staging/.env ]; then
+        cp /var/app/staging/.env.example /var/app/staging/.env
         echo ".env file created from .env.example"
       fi
