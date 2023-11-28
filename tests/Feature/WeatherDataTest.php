37b81d16fac812afe2daeb07edb222e5b8e3219f
stevewolfe5555 <stevewolfe71@gmail.com>
2023-11-28 22:26:22 +0000

    First Test for Fetchin weather data from an api

---
+++ tests/Feature/WeatherDataTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class WeatherDataTest extends TestCase
+{
+    /** @test */
+    public function it_can_retrieve_weather_data_for_a_location()
+    {
+        $response = $this->get('/api/weather?location=Paris');
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'location',
+            'temperature',
+            'conditions'
+        ]);
+    }
+}
